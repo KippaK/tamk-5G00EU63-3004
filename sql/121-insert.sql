502caf6bb37beaef1a687effa1a41c5f3e893620
Antti Venetjoki <antti.venetjoki@gmail.com>
2024-02-15 01:52:18 +0200

    insert excercise done

---
+++ sql/121-insert.sql
@@ -0,0 +1,11 @@
+-- Author: Antti Venetjoki <antti.venetjoki@tuni.fi>
+-- Date: 2024-02-15
+-- File: 121-insert.sql
+
+INSERT INTO person (id, last, first, phone, zip, city, address)
+VALUES
+    (1, 'Doe', 'John', '123455', '11122', 'New York', '2th avenue'),
+    (2, 'Jordan', 'Mike', '221122', '01212', 'Washington', 'South Park 3'),
+    (3, 'Durak', 'Stephen', '550011', '55654', 'Miami', 'Sea Drive 112');
+
+-- End of file
---
+++ sql/122-insert.sql
@@ -0,0 +1,11 @@
+-- Author: Antti Venetjoki <antti.venetjoki@tuni.fi>
+-- Date: 2024-02-15
+-- File: 122-insert.sql
+
+INSERT INTO football (id, first, last, team, comment)
+VALUES
+    (1, 'Jim', 'Anders', 'Manchester', 'UPS 10'),
+    (2, 'Mark', 'Knofles', 'Liverpool', 'Rockwell international'),
+    (3, 'Jenny', 'Richards', 'Highland', 'Scottish national');
+
+-- End of file
