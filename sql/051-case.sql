3ad1a6f5b163339187852d660102b024ed6d07b7
Antti Venetjoki <antti.venetjoki@gmail.com>
2024-02-15 00:31:27 +0200

    case excercise done

---
+++ sql/051-case.sql
@@ -0,0 +1,18 @@
+-- Author: Antti Venetjoki <antti.venetjoki@tuni.fi>
+-- Date: 2024-02-15
+-- File: 051-case.sql
+
+UPDATE emp
+SET job = 'SALES PERSON'
+WHERE UPPER(job) = 'SALESMAN';
+
+SELECT
+    ename AS Name,
+    job AS 'New Job Name'
+FROM
+    emp
+ORDER BY
+    job ASC,
+    ename ASC;
+
+-- End of file
---
+++ sql/052-case.sql
@@ -0,0 +1,21 @@
+-- Author: Antti Venetjoki <antti.venetjoki@tuni.fi>
+-- Date: 2024-02-15
+-- File: 052-case.sql
+
+UPDATE emp
+SET comm = CASE
+    WHEN deptno = 30 AND (comm IS NULL OR comm = 0) THEN 100
+    WHEN deptno = 30 THEN comm * 1.05
+    ELSE comm
+END;
+
+SELECT
+    ename,
+    comm
+FROM
+    emp
+ORDER BY
+    comm ASC,
+    ename ASC;
+
+-- End of file
