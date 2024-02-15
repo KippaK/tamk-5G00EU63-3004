b88f707941b9b9675f76ee5efb6cc674fb7a48e2
Antti Venetjoki <antti.venetjoki@gmail.com>
2024-02-15 07:46:23 +0200

    maybe

--- sql/104-multiple-tables-self-join.sql
+++ sql/104-multiple-tables-self-join.sql
@@ -5,18 +5,19 @@
 SELECT
     d.loc AS Location,
     d.dname AS Department,
-    e1.ename AS Employee,
+    e.ename AS Employee,
     m.ename AS Manager
 FROM
-    emp AS e1
-JOIN emp AS m ON e1.mgr = m.empno
-JOIN dept AS d ON e1.deptno = d.deptno
+    emp AS e
+JOIN emp AS m ON e.mgr = m.empno
+JOIN dept AS d ON e.deptno = d.deptno
+JOIN salgrade AS s ON m.sal BETWEEN s.losal AND s.hisal
 WHERE
-    (UPPER(m.ename) IN ('BLAKE', 'FORD', 'JONES'))
-    AND (m.sal > (SELECT hisal FROM salgrade WHERE grade = 3))
+    UPPER(m.ename) IN ('BLAKE', 'FORD', 'JONES')
+    AND s.grade > 3
 ORDER BY
     d.loc ASC,
     m.ename ASC,
-    e1.ename ASC;
+    e.ename ASC;
 
 -- End of file
